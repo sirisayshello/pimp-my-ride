595dfae302d386091eac95d76de99e87bdf1127c
sirisayshello <veckovis.salongen0p@icloud.com>
2024-03-04 13:52:57 +0100

    added feature form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,3 +9,20 @@
     <button type="submit">Add feature</button>
 </form>
 @include('errors')
+
+<ul>
+@foreach($user->features as $feature)
+<li>
+@if($feature->completed)
+    <s>{{ $feature->desciption }}</s>
+    @else
+    <form action="/features/{{$feature->id}}/complete" method="post">
+        @csrf
+        @method('patch')
+        {{ $feature->description }}
+        <button type="submit">Complete</button>
+    </form>
+    @endif
+</li>
+@endforeach
+</ul>
