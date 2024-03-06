636c8378e3cf035911e60bf6387b8716b83f3cc1
Adam Welin <welinadam@users.noreply.github.com>
2024-03-06 13:33:51 +0100

    CarTest

---
+++ tests/Feature/CarTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class CarTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_create_car()
+    {
+        $user = new User();
+        $user->name = 'Mr Mtv';
+        $user->email = 'fan@mtv.com';
+        $user->password = Hash::make('123');
+        $user->save();
+
+        $this
+            ->actingAs($user)
+            ->post('cars', [
+                'car_description' => 'Finish writing this test'
+            ]);
+
+        $this->assertDatabaseHas('cars', ['car_description' => 'Finish writing this test']);
+    }
+}
